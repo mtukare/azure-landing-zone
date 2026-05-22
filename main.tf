11d27f4f862f40fb7c7f27f3d162aef86245106a
Madhuri <tukare.madhuri@gmail.com>
2026-05-22 14:28:35 +0530

    test new cicd

---
+++ main.tf
@@ -0,0 +1,24 @@
+resource "azurerm_resource_group" "landing_zone" {
+  name     = "rg-landing-zone"
+  location = var.location
+
+}
+
+
+module "networking" {
+  source              = "./modules/networking"
+  resource_group_name = azurerm_resource_group.landing_zone.name
+  location            = var.location
+  vnet_address_space  = var.vnet_address_space
+
+}
+
+resource "azurerm_key_vault" "main" {
+  name                = "kv-portfolio-${var.environment}"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.landing_zone.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "standard"
+}
+
+
