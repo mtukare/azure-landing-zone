9817cb16dbbd2163ef64b90e199d7a984a374ca7
Madhuri <tukare.madhuri@gmail.com>
2026-05-23 21:46:03 +0530

    restructuring the folders

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-tfstate"
-    storage_account_name = "sttfstate220526"
-    container_name       = "tfstate"
-    key                  = "landing-zone.tfstate"
-  }
-}
