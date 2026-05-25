f5c556e565fbb434256a664aa5a5b89104950f03
Madhuri <tukare.madhuri@gmail.com>
2026-05-25 15:02:02 +0530

    fix issues

--- project1-landing-zone/main.tf
+++ project1-landing-zone/main.tf
@@ -19,7 +19,7 @@ module "networking" {
 }
 
 resource "azurerm_key_vault" "main" {
-  name                = "kv-portfolio-${var.environment}"
+  name                = "kv-portfolio-${random_id.kv_suffix.hex}"
   location            = var.location
   resource_group_name = azurerm_resource_group.landing_zone.name
   tenant_id           = data.azurerm_client_config.current.tenant_id
