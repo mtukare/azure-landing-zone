750dcf41c18e03ed7b5b4cf3e60353f9f22f814c
Madhuri <tukare.madhuri@gmail.com>
2026-06-04 21:02:24 +0530

    improving CDN

--- project3-monitoring/main.tf
+++ project3-monitoring/main.tf
@@ -1,46 +1,5 @@
-resource "azurerm_resource_group" "CDN" {
-  name     = "rg-cdn"
+resource "azurerm_resource_group" "main" {
+  name     = "rg-portfolio-site"
   location = var.location
-
-
 }
 
-
-module "networking" {
-  source              = "./modules/networking"
-  resource_group_name = azurerm_resource_group.CDN.name
-  location            = var.location
-  vnet_address_space  = var.vnet_address_space
-
-}
-
-resource "azurerm_key_vault" "main" {
-  name                = "kv-portfolio-${random_id.kv_suffix.hex}"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.CDN.name
-  tenant_id           = data.azurerm_client_config.current.tenant_id
-  sku_name            = "standard"
-
-
-}
-
-resource "azurerm_storage_account" "main" {
-  name                = "stlandingzone${random_id.sa_suffix.hex}"
-  resource_group_name = azurerm_resource_group.CDN.name
-  location            = var.location
-
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  tags = {
-    environment = "monitoring"
-    project     = "monitoring"
-  }
-}
-
-resource "azurerm_storage_account_static_website" "main" {
-  storage_account_id = azurerm_storage_account.main.id
-
-  index_document     = "index.html"
-  error_404_document = "404.html"
-}
--- project3-monitoring/site.tf
+++ project3-monitoring/site.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "site" {
-  name                     = "stportfolio${random_id.sa_suffix.hex}" # must be globally unique
+  name                     = "stportfoliomtukare" #"stportfolio${random_id.sa_suffix.hex}" # must be globally unique
   resource_group_name      = azurerm_resource_group.main.name
   account_tier             = "Standard"
   account_replication_type = "LRS"
